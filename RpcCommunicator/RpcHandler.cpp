bb3e5d770669bae446df614cd21dcc0b1ea96a1b
hihiri <35995497+hihiri@users.noreply.github.com>
2024-06-15 13:39:55 +0200

    Bug fix: UnWrap index and offset

--- RpcCommunicator/RpcHandler.cpp
+++ RpcCommunicator/RpcHandler.cpp
@@ -11,9 +11,18 @@ string RpcHandler::Wrap() {
 	return declareRpcVersion + RpcVersion + declareMethod + Method + declareParams + Params + declareId + to_string(Id) + "}";
 }
 
+int findEndIndex(string input, string declaration) {
+	return input.find(declaration, declaration.length());
+}
+
 //TODO: verify the indexes and offsets work fine
 void RpcHandler::UnWrap(string input) {
-	Method = input.substr(input.find(declareRpcVersion, declareRpcVersion.length()), input.find(declareMethod));
-	Params = input.substr(input.find(declareParams, declareParams.length()), input.find(declareId));
-	Id = stoi(input.substr(input.find(declareId, declareId.length()), input.length() - 1 - input.find(declareId, declareId.length()) - 1));
+	//extract method
+	Method = input.substr(findEndIndex(input, declareRpcVersion)+1, input.find(declareMethod));
+	//extract params
+	int firstIndex = findEndIndex(input, declareParams) + 1;
+	Params = input.substr(firstIndex, input.find(declareId) - firstIndex);
+	//extract JsonId: lenght of id = input.length - 1 (for last '}') - first index
+	int firstIndex = findEndIndex(input, declareId) + 1;
+	Id = stoi(input.substr(firstIndex, input.length() - 1 - firstIndex));
 }
