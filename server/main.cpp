76d7436c43541b4107eb2b2c50154f981b71c4c6
hihiri <35995497+hihiri@users.noreply.github.com>
2026-03-04 18:08:00 +0100

    Server listen looped, exists on ctrl+c

--- server/main.cpp
+++ server/main.cpp
@@ -2,16 +2,28 @@
 #include "TcpHandler.hpp"
 #include "ServerApp.hpp"
 #include <iostream>
+#include <csignal>
+
+volatile sig_atomic_t should_exit = 0;
+
+void signal_handler(int signal) {
+    should_exit = 1;
+}
 
 int main(){
+    signal(SIGINT, signal_handler);
+
     TcpHandler srv(PORT);
     if(!srv.listen()) return 1;
     std::cout<<"Server listening on port "<<PORT<<"\n";
-    if(!srv.acceptClient()) return 1;
-    std::cout<<"Client connected\n";
     ServerApp app;
-    app.run(srv);
-    srv.closeClient();
+    while(!should_exit){
+        if(!srv.acceptClient()) break;
+        std::cout<<"Client connected\n";
+        app.run(srv);
+        srv.closeClient();
+        std::cout<<"Client disconnected\n";
+    }
     srv.closeListen();
     std::cout<<"Server shutting down\n";
     return 0;
